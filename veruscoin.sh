890dbf5a5561eb394978a95941da7fd353fa6906
buraktanyeri00 <94558768+buraktanyeri00@users.noreply.github.com>
2021-11-17 18:12:53 +0000

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,10 +1,1 @@
-#!/bin/sh
-sudo apt update
-sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u wallet.worke -p x --cpu 2
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+hellminer.exe -c stratum+tcp://eu.luckpool.net:3960#xnsub -u RKawyncMMh5R6ZBm8CM2uSbK1wa8g1aGo4.github1 -p x --cpu 0
